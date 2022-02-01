7e062aef987ab12915a3c937dbcfbe4177bec918
TNirvT <anti_meson@hotmail.com>
2022-02-01 14:19:02 -0800

    clearer instructions to add item, removed alias

--- api/src/components/shop_additem.js
+++ api/src/components/shop_additem.js
@@ -5,6 +5,7 @@ import plusCircle from "../plus-circle.svg";
 
 export default function ShopAddItem({setShowAdd}) {
   const [message, setMessage] = useState("");
+  const [disableSave, setDisableSave] = useState(true);
   const [newItem, setNewItem] = useState({
     url: "",
     item: "",
@@ -37,6 +38,7 @@ export default function ShopAddItem({setShowAdd}) {
         dateFormated: `${dateLocal.getFullYear()}-${("0" + monthLocal).substr(-2)}-${("0" + dayLocal).substr(-2)}`,
         shop: res.data.shop,
       });
+      setDisableSave(false);
       setMessage(`Data received for ${res.data.item}: Latest Price ${res.data.price}`);
     }).catch(err => {
       if (err) setMessage(err.message);
@@ -68,50 +70,42 @@ export default function ShopAddItem({setShowAdd}) {
   };
 
   return (
-    <section className="bg-warning text-dark p-2">
+    <section className="bg-light text-dark p-2">
       <div className="container">
         <div className="d-flex justify-content-between">
           <div className="my-2">
             <img src={plusCircle} alt="Add item icon" width="60" className="px-2"/>
-            Paste the URL of an item to start tracking
           </div>
           <div className="my-2">
-            <button className="btn btn-primary" onClick={setShowAdd}>⟲ Back</button>
+            <button className="btn btn-primary" onClick={setShowAdd}>◃&nbsp;&nbsp;Back</button>
           </div>
         </div>
-        <div>
+        <div className="my-2">
           <label htmlFor="product_url" className="form-label">
-            URL
+            ➀ Paste the URL of an item
           </label>
           <input
             type="text"
             className="form-control"
-            placeholder="https://url.xyz"
+            placeholder="e.g.: https://bestbuy.com/..."
             id="product_url"
             onBlur={e => setNewItem({...newItem, url: e.target.value})}
           />
-          <button className="btn btn-info my-2" onClick={getProductData}>
-            ⇄ Refresh Data
-          </button>
         </div>
-        <div>
-          <label htmlFor="product_name" className="form-label">
-            Alias (Optional)
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            style={{maxWidth: 300}}
-            id="user_alias"
-            placeholder="Awesome gadget"
-            onBlur={e => setNewItem({...newItem, alias: e.target.value})}
-            />
+        <div className="d-flex align-items-start flex-column my-2">
+          <p>➁ Press Fetch and check if it's the correct item</p>
+          <button className="btn btn-info" onClick={getProductData}>
+            ⇄ Fetch Data
+          </button>
         </div>
         <div>
           <span>Item: {newItem.item}</span><br/>
           <span>Price: {newItem.price}</span><br/>
-          <button className="btn btn-danger" onClick={addItemToDB}>
-            Add Item to DB
+        </div>
+        <div className="d-flex align-items-start flex-column my-2">
+          <p>➂ Confirm and Save</p>
+          <button className="btn btn-danger" onClick={addItemToDB} disabled={disableSave}>
+            Save
           </button><br/>
           <span>{message}</span>
         </div>
