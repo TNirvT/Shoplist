c1a7f90c9df44d784273b3c372c3e9845c386b7f
TNirvT <anti_meson@hotmail.com>
2022-02-04 15:43:28 -0800

    fix foooter links to open new tab

--- api/src/components/footer.js
+++ api/src/components/footer.js
@@ -9,22 +9,22 @@ export default function Footer() {
       <div className="container">
         <div className="d-flex justify-content-between">
           <div>
-            <a href="https://github.com/TNirvT/" className="link-light link-nodeco">© 2022 TNirvT</a>
+            <a href="https://github.com/TNirvT/" target="_blank" className="link-light link-nodeco">© 2022 TNirvT</a>
           </div>
           <div>
-            <a href="https://github.com/TNirvT" className="link-light link-nodeco"><img src={iconGithub} alt="Github" width="40" className="px-2"/>Github</a>
+            <a href="https://github.com/TNirvT" target="_blank" className="link-light link-nodeco"><img src={iconGithub} alt="Github" width="40" className="px-2"/>Github</a>
           </div>
           <div>
-            <a href="https://linkedin.com" className="link-light link-nodeco"><img src={iconLinkedin} alt="LinkedIn" width="40" className="px-2"/>LinkedIn</a>
+            <a href="https://linkedin.com" target="_blank" className="link-light link-nodeco"><img src={iconLinkedin} alt="LinkedIn" width="40" className="px-2"/>LinkedIn</a>
           </div>
           <div>
-            <a href="https://github.com/TNirvT/Shoplist" className="link-light link-nodeco"><img src={iconRepo} alt="Repository" width="40" className="px-2"/>Repository</a>
+            <a href="https://github.com/TNirvT/Shoplist" target="_blank" className="link-light link-nodeco"><img src={iconRepo} alt="Repository" width="40" className="px-2"/>Repository</a>
           </div>
         </div>
       </div>
       <div className="container my-3">
         Icons by authors on <a href="https://www.svgrepo.com" className="link-light link-nodeco">svgrepo.com</a><br/>
-        <a href="https://github.com/TNirvT/Shoplist/blob/dev/Resources.md" className="link-light link-nodeco">Details.</a>
+        <a href="https://github.com/TNirvT/Shoplist/blob/dev/Resources.md" target="_blank" className="link-light link-nodeco">Details.</a>
       </div>
     </footer>
   )
