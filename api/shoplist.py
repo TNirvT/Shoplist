aa2fd0d29ac2a8bb5d40d516bc731bc8e7682cf3
TNirvT <anti_meson@hotmail.com>
2022-02-04 15:42:42 -0800

    update request headers

--- api/shoplist.py
+++ api/shoplist.py
@@ -90,15 +90,15 @@ def scrap_product_data(url: str, shop: str):
     headers = {
         "accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
         "accept-encoding":"gzip, deflate, br",
-        "accept-language":"en-GB,en;q=0.9",
+        "accept-language":"en-GB,en-US;q=0.9,en;q=0.8",
         "cache-control": "max-age=0",
         "sec-fetch-dest":"document",
         "sec-fetch-mode":"navigate",
-        "sec-fetch-site":"none",
+        "sec-fetch-site":"cross-site",
         "sec-fetch-user":"?1",
         "sec-gpc":"1",
         "Upgrade-Insecure-Requests":"1",
-        "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36",
+        "User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.87 Safari/537.36",
     }
 
     r = requests.get(url, headers=headers)
